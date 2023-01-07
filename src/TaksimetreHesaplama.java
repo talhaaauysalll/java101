8623227e37d48b1c570dd9117cf73ecf732ebaf5
talhaaauysalll <talha.uysal@outlook.com.tr>
2023-01-07 22:28:37 +0300

    Taksimetre hesaplama uygulaması gerçekleştirildi.

---
+++ src/TaksimetreHesaplama.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TaksimetreHesaplama {
+    public static void main(String[] args) {
+        double odenecekTutar,hesaplananTutar,katedilenYol,acilisUcreti=10,kmBasinaFiyat=2.2;
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Gidilen Yol (KM)= ");
+        katedilenYol=input.nextDouble();
+
+        hesaplananTutar=(katedilenYol*kmBasinaFiyat)+acilisUcreti;
+        odenecekTutar=(hesaplananTutar<20)? odenecekTutar=20:hesaplananTutar;
+
+        System.out.print("Ödenecek Olan Tutar= "+odenecekTutar);
+
+
+
+    }
+}
