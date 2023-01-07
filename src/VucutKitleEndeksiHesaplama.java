5bc26ec55fc8b4813ed6b96f3332d3882e404424
talhaaauysalll <talha.uysal@outlook.com.tr>
2023-01-07 23:01:27 +0300

    Vücut kitle endeksi hesaplama uygulaması gerçekleştirildi.

---
+++ src/VucutKitleEndeksiHesaplama.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VucutKitleEndeksiHesaplama {
+    public static void main(String[] args) {
+        double kilo,boy,vki;
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz= ");
+        boy=input.nextDouble();
+
+        System.out.print("Lütfen kilonuzu giriniz= ");
+        kilo=input.nextDouble();
+
+        vki=kilo / (boy*boy);
+        System.out.print("Vücut Kitle Endeksiniz= "+vki);
+
+    }
+}
