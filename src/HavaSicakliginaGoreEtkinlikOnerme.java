46fb5b7a2b7bd90ab39dfd49548c6ff7f32d58aa
talhaaauysalll <talha.uysal@outlook.com.tr>
2023-01-10 22:41:18 +0300

    Hava sıcaklığına göre etkinlik önerme uygulaması gerçekleştirildi.

---
+++ src/HavaSicakliginaGoreEtkinlikOnerme.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HavaSicakliginaGoreEtkinlikOnerme {
+    public static void main(String[] args) {
+        int sicaklik;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Sıcaklık Değerini Giriniz: ");
+        sicaklik=input.nextInt();
+
+        if(sicaklik<5){
+
+            System.out.print("Kayak yapabilirsiniz...");
+        } else if (sicaklik>=5 && sicaklik<15) {
+            System.out.print("Sinemaya gidebilirsiniz...");
+        } else if (sicaklik>=15 && sicaklik<25) {
+            System.out.print("Pikniğe gidebilirsiniz...");
+        } else if (sicaklik>=25) {
+            System.out.print("Yüzmeye gidebilirsiniz.");
+        }
+
+
+    }
+}
