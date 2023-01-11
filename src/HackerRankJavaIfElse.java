b0c349c06d701d8fdd5327eed230de3af728e47e
talhaaauysalll <talha.uysal@outlook.com.tr>
2023-01-11 21:48:24 +0300

    HackerRank'de yer alan bir challenge'a ait program.

---
+++ src/HackerRankJavaIfElse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HackerRankJavaIfElse {
+    public static void main(String[] args) {
+        int N;
+
+        Scanner input=new Scanner(System.in);
+        N=input.nextInt();
+        
+        if(N%2==1){
+            System.out.print("Weird");
+        } else if (N%2==0 && (N>=2 && N<=5)) {
+            System.out.print("Not Weird");
+        } else if (N%2==0 && (N>=6 && N<=20)) {
+            System.out.print("Weird");
+        } else if (N%2==0 && N>20) {
+            System.out.print("Not Weird");
+        }
+    }
+}
