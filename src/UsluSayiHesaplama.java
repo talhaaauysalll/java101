d6c009c42b8108e0fecfe21a66b2297a9573985c
talhaaauysalll <talha.uysal@outlook.com.tr>
2023-01-14 22:08:01 +0300

    Üslü sayı hesaplama uygulaması gerçekleştirildi.

---
+++ src/UsluSayiHesaplama.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UsluSayiHesaplama {
+    public static void main(String[] args){
+        int us,kuvvet,sonuc=1;
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Üssü alınacak sayıyı giriniz: ");
+        us=input.nextInt();
+        System.out.print("Üs olacak sayıyı giriniz: ");
+        kuvvet=input.nextInt();
+
+        for (int i=1;i<=kuvvet;i++){
+            sonuc*=us;
+            System.out.println(us+"^"+i+"="+sonuc);
+        }
+
+    }
+}
