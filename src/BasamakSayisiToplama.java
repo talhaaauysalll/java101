35707316004073ff1af7543c33ebf09386232726
talhaaauysalll <talha.uysal@outlook.com.tr>
2023-01-14 23:08:27 +0300

    Basamak sayısına göre toplama işlemi gerçekleştirildi.

---
+++ src/BasamakSayisiToplama.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BasamakSayisiToplama {
+    public static void main(String[] args) {
+        int sayi,basamakDegeri,toplam=0;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Sayıyı Giriniz: ");
+        sayi=input.nextInt();
+        while (sayi!=0){
+            basamakDegeri=sayi%10;
+            toplam+=basamakDegeri;
+            sayi/=10;
+        }
+        System.out.println(toplam);
+    }
+}
