74c14c0a428ffb45b8f8cf1647f6d5db4e1b6b8a
talhaaauysalll <talha.uysal@outlook.com.tr>
2023-01-14 21:17:16 +0300

    Girilen sayıya kadar olan 4 ve 5'in ussu olan program geliştirildi.

---
+++ src/GirilenSayiyaKadarSayilarinUssunuAlma.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GirilenSayiyaKadarSayilarinUssunuAlma {
+    public static void main(String[] args) {
+        int girilenSayi,dordunUssu=1,besinUssu=1;
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Sayıyı Giriniz: ");
+        girilenSayi=input.nextInt();
+
+        for (int i=1;i<=girilenSayi;i++){
+            dordunUssu=dordunUssu*4;
+            System.out.println("4^"+i+"="+dordunUssu);
+
+        }
+        System.out.println("-------------");
+        for (int i=1;i<=girilenSayi;i++){
+            besinUssu=besinUssu*5;
+            System.out.println("5^"+i+"="+besinUssu);
+        }
+
+
+    }
+}
