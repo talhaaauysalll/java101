550eff7192dbbc93137eac5e95d6a5debd049e7a
talhaaauysalll <talha.uysal@outlook.com.tr>
2023-02-27 22:10:24 +0300

    Tekrar eden çift sayılar hesaplandı.

---
+++ src/Arrays/TekrarEdenCiftSayilariBulma.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class TekrarEdenCiftSayilariBulma {
+    static boolean isFind(int[] arr,int value){
+        for (int i:arr){
+            if(i==value){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] list={2,7,7,3,2,9,2,6,6,6,8,9,12,3};
+        int[] duplicate=new int[list.length];
+        int startIndex=0;
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if ((i != j)&&(list[i]==list[j])&&(list[i]%2==0)) {
+                        if (!isFind(duplicate,list[i])) {
+                            duplicate[startIndex++]=list[i];
+                        }
+                        break;
+                }
+            }
+        }
+        for(int value:duplicate){
+            if(value!=0){
+                System.out.println(value);
+            }
+        }
+    }
+
+}
